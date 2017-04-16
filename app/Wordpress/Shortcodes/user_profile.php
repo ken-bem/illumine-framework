e223056be74b53e801b07dd2aa54628b4411eea1
Ulection.com <webmaster@ulection.com>
2017-04-15 20:21:16 -0700

    init commit

--- app/Wordpress/Shortcodes/user_profile.php
+++
@@ -1,7 +0,0 @@
-<?php /** @var \IllumineFramework\WpShortcode $shortcodes **/
-$shortcodes->add(
-    'illumine_user_profile',
-    'WppSkeleton\Http\Controllers\UserProfileController@load',
-    array('search' => 'Find Plugins...'),
-    array('wpp-search')
-);
